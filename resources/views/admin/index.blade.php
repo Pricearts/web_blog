5e8a6f84356585f5bbcb5fe10ac3e1b970019213
PriceArts <pdw90@mail.ru>
2023-08-30 22:50:31 +0700

    Dashboard index front-end

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,58 @@
 <x-app-layout>
-
+    <main class="container mt-16">
+        <h1 class="text-[22px] font-bold opacity-[80%]">Статьи</h1>
+        <section id="articles" class="mt-8 space-y-8">
+            <article class="border border-slate-300 py-3 px-4 rounded-xl border-opacity-[76%] w-full flex items-center justify-between">
+                <section id="start" class="flex items-center space-x-4">
+                    <img class="w-14 h-14 rounded-full" src="https://tailwindcss.com/_next/static/media/sarah-dayan.de9b3815.jpg" alt="article_creator">
+                    <div>
+                        <h1 class="font-medium text-[19px]">@ntsg</h1>
+                        <p class="text-[#0F1419] opacity-[42%]">3 часа назад</p>
+                    </div>
+                </section>
+                <section id="center">
+                    <h1 class="font-medium opacity-[90%] text-[18px]">Лучшая разработка</h1>
+                    <p class="text-[16px] opacity-[70%]">Lorem ispum dolor sit amet...</p>
+                </section>
+                <section id="end" class="flex items-center space-x-4">
+                    <a href="#" class="text-blue-500">Редактировать</a>
+                    <a href="#" class="text-rose-500">Удалить</a>
+                </section>
+            </article>
+            <article class="border border-slate-300 py-3 px-4 rounded-xl border-opacity-[76%] w-full flex items-center justify-between">
+                <section id="start" class="flex items-center space-x-4">
+                    <img class="w-14 h-14 rounded-full" src="https://tailwindcss.com/_next/static/media/sarah-dayan.de9b3815.jpg" alt="article_creator">
+                    <div>
+                        <h1 class="font-medium text-[19px]">@ntsg</h1>
+                        <p class="text-[#0F1419] opacity-[42%]">3 часа назад</p>
+                    </div>
+                </section>
+                <section id="center">
+                    <h1 class="font-medium opacity-[90%] text-[18px]">Лучшая разработка</h1>
+                    <p class="text-[16px] opacity-[70%]">Lorem ispum dolor sit amet...</p>
+                </section>
+                <section id="end" class="flex items-center space-x-4">
+                    <a href="#" class="text-blue-500">Редактировать</a>
+                    <a href="#" class="text-rose-500">Удалить</a>
+                </section>
+            </article>
+            <article class="border border-slate-300 py-3 px-4 rounded-xl border-opacity-[76%] w-full flex items-center justify-between">
+                <section id="start" class="flex items-center space-x-4">
+                    <img class="w-14 h-14 rounded-full" src="https://tailwindcss.com/_next/static/media/sarah-dayan.de9b3815.jpg" alt="article_creator">
+                    <div>
+                        <h1 class="font-medium text-[19px]">@ntsg</h1>
+                        <p class="text-[#0F1419] opacity-[42%]">3 часа назад</p>
+                    </div>
+                </section>
+                <section id="center">
+                    <h1 class="font-medium opacity-[90%] text-[18px]">Лучшая разработка</h1>
+                    <p class="text-[16px] opacity-[70%]">Lorem ispum dolor sit amet...</p>
+                </section>
+                <section id="end" class="flex items-center space-x-4">
+                    <a href="#" class="text-blue-500">Редактировать</a>
+                    <a href="#" class="text-rose-500">Удалить</a>
+                </section>
+            </article>
+        </section>
+    </main>
 </x-app-layout>
