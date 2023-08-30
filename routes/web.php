ae97bf471f0aa3956985448fd7549ecc491e9461
PriceArts <pdw90@mail.ru>
2023-08-30 11:46:29 +0700

    Initialize base routing

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/article/{slug}', function () {
+    return view('article');
+})->name('article');
+
